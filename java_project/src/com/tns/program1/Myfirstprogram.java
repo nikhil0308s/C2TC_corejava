0b59aab88bd0bd71f1fcdb65d866aea9d16a278e
jeevu <jeevu@DESKTOP-R3Q3S8I>
2022-09-07 17:23:25 +0530

    first commit

---
+++ java_project/src/com/tns/program1/Myfirstprogram.java
@@ -0,0 +1,11 @@
+package com.tns.program1;
+
+public class Myfirstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helo nikhil");
+
+	}
+
+}
